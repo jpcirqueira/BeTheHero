5d0c7af4003c040cf1ff1f078baa8d0e6e741de9
João Soares <jpcirqueira81@gmail.com>
2020-03-28 20:19:20 -0300

    concluida as funcionalidades.

--- mobile/src/pages/detail/index.js
+++ mobile/src/pages/detail/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
 import { View, Text, TouchableOpacity,Image, Linking } from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import {useNavigation, useRoute } from '@react-navigation/native';
 import * as Mailcomposer from 'expo-mail-composer';
 
 import logoImg from '../../assets/logo.png';
@@ -10,22 +10,36 @@ import styles from './styles';
 
 export default function Detail() {
 
+    const route = useRoute();
     const navigation = useNavigation();
-    const message = 'Olá joao, estou entrando em contato pois gostaria de ajudar no caso "Favela solidaria" com o valor de R$ 160 ';
+    const incident = route.params.incident;
+
+    const message = `Olá ${
+        incident.name
+      }, estou entrando em contato pois gostaria de ajudar no caso"${
+        incident.title
+      }" com o valor de ${Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+      }).format(incident.value)}`;
+    
+
+    
+
     function navigateBack() {
         navigation.goBack();
     }
 
     function sendMail() {
         Mailcomposer.composeAsync({
-            subject: "Herói do caso: Favela solidaria",
-            recipients: ['jpciqueira81@gmail.com'],
+            subject: `Herói do caso: ${incident.title}`,
+            recipients: [incident.email],
             body: message,
         })
     }
 
     function sendWhatsApp() {
-        Linking.openURL(`whatsapp://send?phone=55612343432&text=${message}`);
+        Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${message}`);
     }
 
 
@@ -39,14 +53,19 @@ export default function Detail() {
             </View>
 
             <View style={styles.incident}>
-                <Text style={styles.incidentProperty}>ONG:</Text>
-                <Text style={styles.incidentValue}>Favela solidaria</Text>
+                <Text style={[styles.incidentProperty, {marginTop: 0}]}>ONG:</Text>
+    <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf}</Text>
 
                 <Text style={styles.incidentProperty}>Caso:</Text>
-                <Text style={styles.incidentValue}>Arrwcadação de alimentos</Text>
+                <Text style={styles.incidentValue}>{incident.title}</Text>
 
                 <Text style={styles.incidentProperty}>Valor:</Text>
-                <Text style={styles.incidentValue}>254</Text>
+                <Text style={styles.incidentValue}>
+                    {Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                    }).format(incident.value)}
+                </Text>
             </View>
             <View style={styles.contactBox}>
                 <Text style={styles.heroTitle}>Salve o dia!</Text>
